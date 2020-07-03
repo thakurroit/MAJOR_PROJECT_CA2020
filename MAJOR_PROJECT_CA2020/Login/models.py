996885647be406177545f1556640e531c1348e9e
thakurroit <54647416+thakurroit@users.noreply.github.com>
2020-07-03 10:53:16 -0700

    Adding the Create User, Login and Logout Functionality

--- MAJOR_PROJECT_CA2020/Login/models.py
+++ MAJOR_PROJECT_CA2020/Login/models.py
@@ -1,9 +1,5 @@
 from django.db import models
 
 # Create your models here.
-class LoginApp(models.Model):
-    Username=models.CharField(max_length=20)
-    Password=models.CharField(max_length=20)
-    Address=models.CharField(max_length=100)
-    Email=models.CharField(max_length=30)
+
 
--- MAJOR_PROJECT_CA2020/Login/views.py
+++ MAJOR_PROJECT_CA2020/Login/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
-def index(request):
-    return HttpResponse('Hey')
+def indexView(request):
+    return render(request,'index.html')
+@login_required()
+def dashboardView(request):
+    return render(request,'dashboard.html')
+
+def registerView(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login_url')
+    else:
+        form = UserCreationForm()
+
+    return render(request,'registration/register.html', {'form':form})
+
+
